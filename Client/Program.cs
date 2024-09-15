9dac99616e252fa4f0cd1a2fce84dfe7f215d43e
Sebastian Gonzalez <devsebasgr@gmail.com>
2024-09-15 14:57:03 -0400

    servidor y client creado

---
+++ Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+class Client {
+    public async Task Start() {
+        var client = new TcpClient("127.0.0.1", 6969);
+        var stream = client.GetStream();
+        var msg = Encoding.ASCII.GetBytes("PING");
+        await stream.WriteAsync(msg);
+
+        var readBuff = new Byte[1024];
+        await stream.ReadAsync(readBuff, 0, readBuff.Length);
+        var data = Encoding.ASCII.GetString(readBuff);
+
+        Console.WriteLine(data);
+    }
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new Client();
+        await client.Start();
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+class Server {
+    public async Task Start() {
+        try {
+            
+            var listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 6969);
+            listener.Start();
+
+            while (true) {
+                var client = await listener.AcceptTcpClientAsync();
+                Byte[] buffer = new Byte[1024];
+
+                var stream = client.GetStream();
+                await stream.ReadAsync(buffer,0,buffer.Length);
+
+                var data = Encoding.ASCII.GetString(buffer);
+
+                Console.WriteLine(data);
+
+                var msg = Encoding.ASCII.GetBytes("PONG");
+                await stream.WriteAsync(msg);
+            }
+        } catch(SocketException e) {
+            Console.WriteLine(e);
+        }
+    }
+
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var server = new Server();
+        await server.Start();
+    }
+}
